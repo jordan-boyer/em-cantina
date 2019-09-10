db75404781534a0b9bceb1b645beff135fe89cdf
Jordan BOYER <boyer.jordan42@gmail.com>
2019-09-10 19:32:46 +0200

    add fetchById

--- src/typescript/recipesServices.ts
+++ src/typescript/recipesServices.ts
@@ -1,10 +1,11 @@
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosResponse, AxiosError } from 'axios';
 
 axios.defaults.baseURL = 'http://localhost:9000/api';
 
-const errorHandler = async (res: AxiosResponse): Promise<Error> => {
-    console.log(res);
-    return Promise.reject(new Error("Error"));
+const errorHandler = async (res: AxiosError): Promise<Error> => {
+    let errorTemplate = "Erreur de communication avec le server";
+    let errorMessage = res.response ? (res.response.data ? res.response.data.errorMessage : errorTemplate) : errorTemplate;
+    return Promise.reject(new Error(errorMessage));
 };
 
 class RecipesServices {
@@ -20,6 +21,15 @@ class RecipesServices {
         } catch (res) {
             return errorHandler(res);
         }   
+    }
+
+    public async getById(id: string): Promise<any> {
+        try {
+            let recipe = await axios.get(`/recipe/${id}`)
+            return recipe.data;
+        } catch (res) {
+            return errorHandler(res);
+        }
     }
 }
 
