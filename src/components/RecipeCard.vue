82bde804418e5b3037a6c264d0f7740700fe9c83
Jordan Boyer <boyer.jordan42@gmail.com>
2019-09-10 11:45:42 +0200

    add boilerplate for RecipeCard

---
+++ src/components/RecipeCard.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h4>Titre</h4>
+        <p>Description</p>
+        <p>Difficulté</p>
+        <p>Personnes</p>
+        <p>Time</p>
+        <p>Steps</p>
+        <img src="" alt="Photo de la recette">
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped scr="../scss/recipeCard.scss"></style>
