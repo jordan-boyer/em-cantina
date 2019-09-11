754f320403e2d6f24209cf9b6d5c7e81e8460854
Jordan BOYER <boyer.jordan42@gmail.com>
2019-09-11 17:53:22 +0200

    add filters interface

---
+++ src/typescript/filters.ts
@@ -0,0 +1,13 @@
+import { Difficulty } from './recipes';
+
+interface IMinMax {
+    min: number,
+    max: number
+}
+
+export interface IFilters {
+    title: string,
+    difficulty: Difficulty | "",
+    nbPersons: IMinMax,
+    time: number
+}
