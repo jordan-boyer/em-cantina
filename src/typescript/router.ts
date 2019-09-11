80d6ab0a80725bb277b3e9464e03b3c6ac48fdbd
Jordan BOYER <boyer.jordan42@gmail.com>
2019-09-11 14:45:49 +0200

    change router mode (history to #)

--- src/typescript/router.ts
+++ src/typescript/router.ts
@@ -1,11 +1,11 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Recipes from '../views/Recipes.vue';
+import Recipe from '../views/Recipe.vue'
 
 Vue.use(Router);
 
 export default new Router({
-    mode: 'history',
     base: process.env.BASE_URL,
     routes: [
         {
@@ -16,10 +16,11 @@ export default new Router({
         {
             path: '/recette/:id',
             name: 'recipe',
+            component: Recipe
             // route level code-splitting
             // this generates a separate chunk (about.[hash].js) for this route
             // which is lazy-loaded when the route is visited.
-            component: async (): Promise<any> => await import(/* webpackChunkName: "recipe" */ '../views/Recipe.vue')
+            //component: async (): Promise<any> => await import(/* webpackChunkName: "recipe" */ '../views/Recipe.vue')
         }
     ]
 });
