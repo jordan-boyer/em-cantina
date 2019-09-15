83b5735b20c5c587caf733be984348fe3ff51512
Jordan Boyer <boyer.jordan42@gmail.com>
2019-09-15 18:10:37 +0200

    add style for recipeCard

--- src/components/RecipeCard.vue
+++ src/components/RecipeCard.vue
@@ -1,21 +1,21 @@
 <template>
-    <div v-if="recipe" @click="checkClick">
-        <h4>{{ recipe.titre }}</h4>
-        <p>Description: {{ recipe.description }}</p>
-        <p>Niveau de difficulté: {{ recipe.niveau }}</p>
-        <p>Pour {{ recipe.personnes }} {{Number(recipe.personnes) > 1 ? "personnes": "personne"}}</p>
-        <p>Temps de préparation: {{ time }}</p>
-        <ul v-if="showAll" class="ingredients">
+    <div v-if="recipe" @click="checkClick" class="recipecard">
+        <h4 class="recipecard-header">{{ recipe.titre }}</h4>
+        <p class="recipecard-description">Description: {{ recipe.description }}</p>
+        <p class="recipecard-difficulty">Niveau de difficulté: {{ recipe.niveau }}</p>
+        <p class="recipecard-person">Pour {{ recipe.personnes }} {{Number(recipe.personnes) > 1 ? "personnes": "personne"}}</p>
+        <p class="recipecard-time">Temps de préparation: {{ time }}</p>
+        <ul v-if="showAll" class="recipecard-ingredients">
             {{recipe.ingredients.length > 1 ? "Ingredients" : "Ingredient"}}:
             <li v-for="ingredient in recipe.ingredients" :key="getIngredient(ingredient)"> {{ getIngredient(ingredient) }}</li>
         </ul>
-        <ul v-if="showAll" class="steps">
+        <ul v-if="showAll" class="recipecard-steps">
             {{recipe.etapes.length > 1 ? "Etapes" : "Etape"}}:
             <li v-for="step in recipe.etapes" :key="step"> {{ step }}</li>
         </ul>
-        <img :src="recipe.photo ? recipe.photo : defaultImg" alt="Photo de la recette">
-        <button @click="editRecipe">Edition</button>
-        <button @click="showModal = true">Supprimé</button>
+        <img class="recipecard-photo" :src="recipe.photo ? recipe.photo : defaultImg" alt="Photo de la recette">
+        <button class="recipecard-btn recipecard-btn--edition" @click="editRecipe">Edition</button>
+        <button class="recipecard-btn recipecard-btn--sup" @click="showModal = true">Supprimé</button>
         <modal v-if="showModal" @close="showModal = false" @ok="deleteRecipe">
             <h3 slot="header">Voulez vous vraiment supprimé cette recette</h3>
             <p slot="body">Attention cette action est irréversible</p>
@@ -25,4 +25,4 @@
 
 <script lang="ts" src="../typescript/recipeCard.ts"></script>
 
-<style lang="scss" scoped scr="../scss/recipeCard.scss"></style>
+<style lang="scss" src="../scss/recipeCard.scss" scoped></style>
