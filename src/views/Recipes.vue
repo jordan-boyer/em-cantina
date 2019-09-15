b5036ed4550a142f81af8cbb71316f3e50372c1e
Jordan Boyer <boyer.jordan42@gmail.com>
2019-09-15 18:55:13 +0200

    add style for home page

--- src/views/Recipes.vue
+++ src/views/Recipes.vue
@@ -1,24 +1,32 @@
 <template>
     <div>
-        <div>
-            <p>Filtres</p>
-            <label for="filter-title">Titre</label>
-            <input type="search" id="filter-title" v-model="filtersTitle">
-            <label for="filter-difficulty">Difficulté</label>
-            <select id="filter-difficulty" v-model="filtersDifficulty">
-                <option value="" selected>Toute difficulté</option>
-                <option :value="difficulty.PADAWAN">{{ difficulty.PADAWAN }}</option>
-                <option :value="difficulty.JEDI">{{ difficulty.JEDI }}</option>
-                <option :value="difficulty.MAITRE">{{ difficulty.MAITRE }}</option>
-            </select>
-            <div>
-                Nombre de personne entre <input type="number" name="X" min="0" v-model="filtersPersonMin"> et <input type="number" name="Y" min="0" v-model="filtersPersonMax">
+        <div class="filter">
+            <p>Filtres: </p>
+            <div class="filter-group">
+                <label for="filter-title">Titre: </label>
+                <input class="filter-input" type="search" id="filter-title" v-model="filtersTitle">
             </div>
-            <label for="filter-time">Temps &lt; en minutes</label>
-            <input type="number" id="filter-time" min="0" v-model="filtersTime">
+            <div class="filter-group">
+                <label for="filter-difficulty">Difficulté: </label>
+                <select id="filter-difficulty" v-model="filtersDifficulty">
+                    <option value="" selected>Toute difficulté</option>
+                    <option :value="difficulty.PADAWAN">{{ difficulty.PADAWAN }}</option>
+                    <option :value="difficulty.JEDI">{{ difficulty.JEDI }}</option>
+                    <option :value="difficulty.MAITRE">{{ difficulty.MAITRE }}</option>
+                </select>   
+            </div>
+            
+            <div class="filter-group">
+                Nombre de personne entre <input class="filter-input" type="number" name="X" min="0" v-model="filtersPersonMin"> et <input class="filter-input" type="number" name="Y" min="0" v-model="filtersPersonMax">
+            </div>
+            <div class="filter-group">
+                <label for="filter-time">Temps &lt; en minutes: </label>
+                <input class="filter-input" type="number" id="filter-time" min="0" v-model="filtersTime">
+            </div>
+            
         </div>
         <div class="grid-list">
-            <recipe-card v-for="recipe in filteredList" :key="recipe.id" :recipe="recipe" @CLICK="goToRecipe(recipe)"/>
+            <recipe-card class="click" v-for="recipe in filteredList" :key="recipe.id" :recipe="recipe" @CLICK="goToRecipe(recipe)"/>
         </div>
     </div>
 </template>
@@ -27,6 +35,6 @@
 
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss" src="../scss/recipesView.scss" scoped>
 
 </style>
