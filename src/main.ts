3fd3f62b5612ec987a7c4cb3ff0386e14b8042ba
Jordan Boyer <boyer.jordan42@gmail.com>
2019-09-15 16:28:38 +0200

    remove serviceWorker for PWA

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import Component from 'vue-class-component';
 import App from './App.vue';
 import router from './typescript/router';
 import store from './typescript/store';
-import './typescript/registerServiceWorker';
 
 Vue.config.productionTip = false;
 
