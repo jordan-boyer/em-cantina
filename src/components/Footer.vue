501dcb233342db1c11bb383fefd5cad79dc5df66
Jordan BOYER <boyer.jordan42@gmail.com>
2019-09-16 13:19:36 +0200

    add footer style

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,7 @@
 <template>
     <footer class="footer">
         <p class="footer-copyright">Créer par jordan Boyer pour l'école multimédia</p>
+        <p class="footer-github">Code source disponible <a class="footer-github--link" href="https://github.com/jordan-boyer/em-cantina">ici</a></p>
     </footer>
 </template>
 
